61648588aa363e0e4ec15254dd3c04d99d74491a
Ashutosh <ashutoshsignh4415@gmail.com>
2025-05-31 01:04:42 +0530

    OOPs project 1

---
+++ OOPs.js
@@ -0,0 +1,58 @@
+// function Toffee(){
+//     this.flavour = "Vanilla"
+//     this.price = 296
+//     this.quality = "good"
+// }
+
+// let t1 = new Toffee()
+// let t2 = new Toffee()
+
+// function Toffee(flavour,price,quality){
+//     this.flavour = flavour
+//     this.price = price
+//     this.quality = quality
+// }
+
+// let t1 = new Toffee("vanilla", 10, "bad")
+// let t2 = new Toffee("Strawberry", 30, "worse")
+
+// class Toffe {
+//   constructor() {
+//     this.flavour = "Vanilla"
+//     this.price = 296
+//     this.quality = "good"
+//   }
+// }
+
+// let t1 = new Toffe()
+
+
+// class Toffe {
+//   constructor(flavour,price,quality) {
+//     this.flavour = flavour
+//     this.price = price
+//     this.quality = quality
+//   }
+// }
+
+// let t1 = new Toffe("vanilla", 10, "bad")
+// let t2 = new Toffe("Strawberry", 30, "worse")
+
+// Prototype-
+
+// function Toffee(flavour){
+//     this.flavour = flavour
+// }
+
+// Toffee.prototype.price = 3
+// Toffee.prototype.review = function(){
+// console.log("mst hai bht")
+// }
+// Toffee.prototype.quality = "Excellent"
+
+// let t1 = new Toffee("vanilla")
+// let t2 = new Toffee("Strawberry")
+
+
+
+
